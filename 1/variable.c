f439d83ac6530eafad46ab6c8d947f49a48649be
Ywoosang <opellong13@gmail.com>
2021-02-23 17:21:26 +0900

    Add char variable

--- 1/variable.c
+++ 1/variable.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 int  main(){
-    int a = 1234; 
-    printf("%d\n",a);
+    int a0 = 1234; 
+    printf("%d\n",a0);
     float a1 = 3.15;
     printf("%f\n",a1);
     char a2 = 'A';
@@ -27,7 +27,27 @@ int  main(){
     double f = 123456789012345;
     float g = f;
     printf("%f\n%f\n",f,g);
-    
+
+    char string;
+    // 문자를 코드에서 작성할 때는 반드시 홑따옴표로 묶어야 한다.
+    string = 'abcd';
+    // 마지막 문자만 출려됌.
+    printf("%c\n",string);
+
+    int a,b =3;
+    a = b+2; //5
+    a = a*2; // 10 
+    a = a-3; // 7
+    a = a/3; // 2 
+    printf("%d\n",a);
+
+    char str = 'A';
+    printf("%d\n",str);  // 65 출력 
+    str = str + 1; // 66 
+    printf("%d\n",str); 
+    printf("%c\n",str); 
+
+
 }
 
 
